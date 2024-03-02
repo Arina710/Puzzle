73ea5034aae0a2d04add889ab01e36a49a2dbed2
Arina710 <144335998+Arina710@users.noreply.github.com>
2024-03-02 15:05:33 +0300

    2 version

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tkinter import PhotoImage
 import tkinter as tk
 
 def on_click(event):
@@ -6,28 +7,33 @@ def on_click(event):
         selected_piece = event.widget
     else:
         if selected_piece != event.widget:
-            temp_text = selected_piece.cget("text")
-            selected_piece.config(text=event.widget.cget("text"))
-            event.widget.config(text=temp_text)
+            selected_image = selected_piece.cget("image")
+            selected_piece.config(image=event.widget.cget("image"))
+            event.widget.config(image=selected_image)
             selected_piece = None
 
 root = tk.Tk()
 root.title("Игра Пазлы")
 
+image1 = PhotoImage(file='1.png')
+image2 = PhotoImage(file='2.png')
+image3 = PhotoImage(file='3.png')
+image4 = PhotoImage(file='4.png')
+
 canvas = tk.Canvas(root, width=600, height=400, bg='white')
 canvas.pack()
 
-piece1 = tk.Label(canvas, text="Пазл 1", bg='lightblue')
+piece1 = tk.Label(canvas, image=image1, bg='white')
 piece1_window = canvas.create_window(100, 50, window=piece1)
 
-piece2 = tk.Label(canvas, text="Пазл 2", bg='lightgreen')
-piece2_window = canvas.create_window(140, 50, window=piece2)
+piece2 = tk.Label(canvas, image=image2, bg='white')
+piece2_window = canvas.create_window(200, 50, window=piece2)
 
-piece3 = tk.Label(canvas, text="Пазл 3", bg='lightpink')
-piece3_window = canvas.create_window(100, 70, window=piece3)
+piece3 = tk.Label(canvas, image=image3, bg='white')
+piece3_window = canvas.create_window(100, 100, window=piece3)
 
-piece4 = tk.Label(canvas, text="Пазл 4", bg='lightyellow')
-piece4_window = canvas.create_window(140, 70, window=piece4)
+piece4 = tk.Label(canvas, image=image4, bg='white')
+piece4_window = canvas.create_window(200, 100, window=piece4)
 
 selected_piece = None
 
